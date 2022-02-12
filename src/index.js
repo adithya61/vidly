64deff354d47c5aa9df10798f040d5286b7f6aef
adithya61 <zonic@Adithyas-MacBook-Air.local>
2022-02-12 23:27:38 +0530

    added movies compoenet

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
+import Movies from './Movies'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Movies />
   </React.StrictMode>,
   document.getElementById('root')
 );
