8e75c32f6ea7b451e4c577c7fe6740b7d92532e5
adithya61 <zonic662@gmail.com>
2023-10-11 02:27:44 +0530

    env var 1

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = process.env.backend_api_url;
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
